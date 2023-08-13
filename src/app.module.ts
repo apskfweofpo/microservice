cf0cd07abe4397e541623413d67edda506b3a7c3
Pavel Krikunov <fusrodah.df@mail.ru>
2023-08-13 17:26:14 +0300

    typeorm connect to db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,9 @@
 import { Module } from '@nestjs/common';
+import { ProvidersModule } from "@lib/providers";
 
 @Module({
-  imports: [],
+  imports: [
+    ProvidersModule,
+  ],
 })
 export class AppModule {}
