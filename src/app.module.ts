c2acdd15166e060f1442a282f7e5eccc820d9b64
Pavel Krikunov <fusrodah.df@mail.ru>
2023-08-14 16:05:05 +0300

    add all exception filter

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ProvidersModule } from "@lib/providers";
+import { SharedModule } from "@lib/shared";
 
 @Module({
   imports: [
+    SharedModule,
     ProvidersModule,
   ],
 })
