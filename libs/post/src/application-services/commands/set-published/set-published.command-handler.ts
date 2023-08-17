1b79e66a7c9fa9f233137c8cbc087de2ae01d9a5
Pavel Krikunov <fusrodah.df@mail.ru>
2023-08-17 20:42:17 +0300

    refactor

--- libs/post/src/application-services/commands/set-published/set-published.command-handler.ts
+++ libs/post/src/application-services/commands/set-published/set-published.command-handler.ts
@@ -11,11 +11,13 @@ import {
 export class SetPublishedCommandHandler
   implements ICommandHandler<SetPublishedCommand, PostAggregate> {
   private readonly logger = new Logger(UpdatePostCommandHandler.name);
-  constructor(private readonly postRepository: PostRepository) {}
 
-  async execute({id}: SetPublishedCommand): Promise<PostAggregate> {
+  constructor(private readonly postRepository: PostRepository) {
+  }
+
+  async execute({ id }: SetPublishedCommand): Promise<PostAggregate> {
     const existPost = await this.postRepository.findOne(id).catch(err => {
-      this.logger.error(err)
+      this.logger.error(err);
       return null as PostAggregate;
     });
     if (!existPost) {
